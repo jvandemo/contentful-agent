6a38eb687ef8a52d45b9d6c3327ba7598127d448
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-04-11 16:53:59 +0200

    test(agent): add unit tests

--- test/agent.js
+++ test/agent.js
@@ -7,6 +7,10 @@ var options = {
 };
 var contentfulAgent = require('../index.js');
 
+beforeEach(function(){
+  request = contentfulAgent(options);
+});
+
 describe('contentful-agent', function(){
 
   it('should return a function', function(){
@@ -33,6 +37,46 @@ describe('contentful-agent', function(){
     var request = contentfulAgent({ space: 'space', accessToken: 'token' });
     expect(request).to.be.an('object');
     expect(request.get).to.be.a('function');
+  });
+
+});
+
+describe('request', function(){
+
+  it('should be an object with a `get` method when valid options are passed', function(){
+    expect(request).to.be.an('object');
+    expect(request.get).to.be.a('function');
+  });
+
+  describe('#get', function(){
+
+    it('should return a rejected promise when a content type without id is passed', function(done){
+      var contentTypes = {
+        cats: {}
+      };
+      var promise = request.get(contentTypes);
+      promise.then(null, function(error){
+        done();
+      });
+    });
+
+    it('should resolve the returned promise with the fetched entries', function(done){
+      var contentTypes = {
+        cats: {
+          id: '63k4qdEi9aI8IQUGaYGg4O'
+        },
+        dogs: {
+          id: 'dog'
+        }
+      };
+      var promise = request.get(contentTypes);
+      promise.then(function(entries){
+        expect(entries.cats.length).to.be.gt(0);
+        expect(entries.dogs.length).to.be.gt(0);
+        done();
+      });
+    });
+
   });
 
 });
